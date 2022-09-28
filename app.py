e67805522939432cb72b25275ef0c9b2f140c4ab
Arsh <lucifer78908@gmail.com>
2022-09-28 10:52:30 +0530

    revert test
    Signed-off-by: Arsh <lucifer78908@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import gradio as gr
-import pytesseract
-
-
-def ocr(image):
-    '''
-    Args:
-        image: PIL Image
-    Returns:
-        string: text in the image
-    '''
-    custom_config = r'--oem 3 --psm 3 '  # setting the OCR engine to Default and the page segmentation mode to auto
-    return pytesseract.image_to_string(image, config=custom_config)
+from ocr import ocr
 
 #Here we are going to use Gradio Blocks
 demo = gr.Blocks()
