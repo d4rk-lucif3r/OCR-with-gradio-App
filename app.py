47fb9bc37fdb3bc82a379981ad81173a62663e16
Arsh <lucifer78908@gmail.com>
2022-09-28 09:28:47 +0530

    Updating Docs
    Signed-off-by: Arsh <lucifer78908@gmail.com>

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 import gradio as gr
 from ocr import ocr
+
+#Here we are going to use Gradio Blocks
 demo = gr.Blocks()
+
 with demo:
+
     gr.Markdown("# OCR Demo")
+
     with gr.Tabs():
-        with gr.TabItem("Examples"):
+        with gr.TabItem("Examples"):  # Example Tab where we will have 2 images
             with gr.Row():
                 rad = gr.components.Radio(
                     ['Example 1', 'Example 2'], label='Select Video and wait till it appears!')
                 img1 = gr.components.Image()
                 web_bt1 = gr.Button('Submit')
-        with gr.TabItem("Upload"):
+
+        with gr.TabItem("Upload"):  # Upload Tab where we can upload any image
             with gr.Row():
                 img2 = gr.components.Image(source='upload')
             web_bt2 = gr.Button('Submit')
 
     def action(choice):
+        '''function for radio component'''
         if choice == 'Example 1':
             return 'test-images/receipt-1.jpeg'
         elif choice == 'Example 2':
             return 'test-images/receipt-2.jpeg'
+
     rad.change(action, rad, img1)
+
     op = gr.components.Textbox(label='Output')
 
     def fn(video):
@@ -29,6 +38,7 @@ with demo:
 
     web_bt2.click(fn, inputs=[img2], outputs=op)
     web_bt1.click(fn, inputs=[img1], outputs=op)
+
 try:
     demo.launch(inline=False, server_name="0.0.0.0",
                 server_port=8080, max_threads=100)
--- ocr.py
+++ ocr.py
@@ -1,4 +1,10 @@
 import pytesseract
 def ocr(image):
-    custom_config = r'--oem 3 --psm 6 '
+    '''
+    Args:
+        image: PIL Image
+    Returns:
+        string: text in the image
+    '''
+    custom_config = r'--oem 1 --psm 3 ' #setting the OCR engine to LSTM and the page segmentation mode to auto
     return pytesseract.image_to_string(image, config=custom_config)
