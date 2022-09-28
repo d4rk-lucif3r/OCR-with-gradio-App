0a26b56638ae1f0c7b1a566d55b6d6ee8bd90637
Arsh <lucifer78908@gmail.com>
2022-09-28 10:46:01 +0530

    Revert OEM
    Signed-off-by: Arsh <lucifer78908@gmail.com>

--- ocr.py
+++ ocr.py
@@ -6,5 +6,5 @@ def ocr(image):
     Returns:
         string: text in the image
     '''
-    custom_config = r'--oem 1 --psm 3 ' #setting the OCR engine to LSTM and the page segmentation mode to auto
+    custom_config = r'--oem 3 --psm 3 ' #setting the OCR engine to Default and the page segmentation mode to auto
     return pytesseract.image_to_string(image, config=custom_config)
